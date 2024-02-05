0c624220e53268fe5ad2b0cf8e06091850ca82fa
Edson Bruno <edsonbruno415@gmail.com>
2024-02-04 23:01:32 -0300

    feat: add create bucket function

--- localstack/docker-plus/src/index.js
+++ localstack/docker-plus/src/index.js
@@ -18,3 +18,51 @@ module.exports.hello = async (event, context) => {
     ),
   };
 };
+
+module.exports.create = async (event, context) => {
+  try {
+    const body = JSON.parse(event.body);
+    if (!body) throw new Error('Need bucket name!');
+
+    const { bucketName } = body;
+    console.log('body', event.body);
+    const bucketParams = {
+      Bucket: bucketName,
+    };
+    await S3.createBucket(bucketParams).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'CREATE BUCKET',
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    if (err.statusCode < 500) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify(
+          {
+            message: `Error on create Bucket: ${err}`,
+          },
+          null,
+          2
+        ),
+      };
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: `Internal server error: ${err}`,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
